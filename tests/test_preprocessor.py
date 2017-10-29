52f6023f6618b4793ef2641b7d8a65808c88779a
Vikram Chandrashekhar <vchandr6@jhu.edu>
2017-10-29 14:34:49 -0400

    updating tests

--- tests/test_preprocessor.py
+++ tests/test_preprocessor.py
@@ -7,7 +7,7 @@ class TestPreprocessor(BaseTestCase):
     
     @classmethod
     def setUpClass():
-        img = tf.imread('../sample_data/atlas_to_control9.tif')
+        img = tf.imread('../sample_data/atlas_to_control_9.tif')
         img_sitk = sitk.GetImageFromArray(img)
         img_sitk.SetSpacing((0.0005, 0.0005, 0.005))
         img_sitk.SetDirection(sitk.AffineTransform(img_sitk.GetDimension()).GetMatrix())
@@ -19,4 +19,11 @@ class TestPreprocessor(BaseTestCase):
     def test_bad_initialization(self):
         preprocessor_bad = Preprocessor(sitk.GetArrayFromImage(img_sitk))
 
-    def test_
+    def test_remove_streak(self):
+
+    def test_create_mask(self):
+
+    def test_remove_circle(self):
+
+    def correct_bias_field(self):
+
