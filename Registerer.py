ae663a6488ce3bb9e5e96f1d13ecf03d2e16d799
Vikram Chandrashekhar <vikramc@jhmi.edu>
2017-10-30 14:36:07 -0400

    bug fixes and updated demo notebook

--- Registerer.py
+++ Registerer.py
@@ -21,11 +21,21 @@ class Registerer:
         img_reoriented = ndreg.imgReorient(self.img, inOrient, refOrient)
         atlas_ds = sitk.Clamp(ndreg.imgResample(self.atlas, spacing), upperBound=ndreg.imgPercentile(self.atlas, 0.99))
         img_ds = sitk.Clamp(ndreg.imgResample(img_reoriented, spacing), upperBound=ndreg.imgPercentile(img_reoriented, 0.99))
+        # normalize
+        max_val = ndreg.imgPercentile(img_ds, 0.999)
+        min_val = ndreg.imgPercentile(img_ds, 0.001)
+        img_ds = (img_ds - min_val)/(max_val - min_val)
+        
+        max_val = ndreg.imgPercentile(atlas_ds, 0.999)
+        min_val = ndreg.imgPercentile(atlas_ds, 0.001)
+        atlas_ds = (atlas_ds - min_val)/(max_val - min_val)
+        
         fixedImage = img_ds
         movingImage = atlas_ds
         
         # set parameters
         affineParameterMap = sitk.GetDefaultParameterMap('affine')
+        affineParameterMap['MaximumNumberOfSamplingAttempts'] = '0'
         affineParameterMap['Metric'] = ['AdvancedMeanSquares']
         affineParameterMap['MaximumNumberOfIterations'] = ['{}'.format(iterations)]
         affineParameterMap['Optimizer'] = ['StandardGradientDescent']
@@ -44,9 +54,10 @@ class Registerer:
     def register_lddmm(self, affine_img, target_img, alphaList=[0.05], scaleList=[0.0625, 0.125, 0.25],
             epsilonList=1e-7, sigma=None, useMI=False, iterations=200, verbose=True):
         if sigma == None:
-            sigma = (0.1/inImg_ds.GetNumberOfPixels())
+            sigma = (0.1/target_img.GetNumberOfPixels())
+        # TODO: Add sigma param in ndreg and recompile
         (field, invField) = ndreg.imgMetamorphosisComposite(affine_img, target_img, alphaList=alphaList,
-                                              scaleList = scaleList, epsilonList=epsilonList, sigma=sigma,
+                                              scaleList = scaleList, epsilonList=epsilonList,
                                               useMI=useMI, iterations=iterations, verbose=verbose)
         affineField = ndreg.affineToField(self.affine, field.GetSize(), field.GetSpacing())
         self.fieldComposite = ndreg.fieldApplyField(field, affineField)
